9733f124e171c5c52c2106d46a995b916cb32ab9
BoodyElemary <abdelrahman_elemary99@outlook.com>
2023-06-23 01:39:18 +0300

    making the structure

---
+++ src/app/pages/dashboard/dashboard-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+import { Product } from 'src/app/models/product';
+// import { ProductService } from '../../service/product.service';
+import { Subscription } from 'rxjs';
+import { LayoutService } from 'src/app/layout/service/app.layout.service';
+@Component({
+  selector: 'app-dashboard-page',
+  templateUrl: './dashboard-page.component.html',
+  styleUrls: ['./dashboard-page.component.scss'],
+})
+export class DashboardPageComponent {}
---
+++ src/app/pages/dashboard/dashboard.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DashboardPageComponent } from './dashboard-page.component';
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { ChartModule } from 'primeng/chart';
+import { MenuModule } from 'primeng/menu';
+import { TableModule } from 'primeng/table';
+import { ButtonModule } from 'primeng/button';
+import { StyleClassModule } from 'primeng/styleclass';
+import { PanelMenuModule } from 'primeng/panelmenu';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: DashboardPageComponent,
+  },
+];
+
+@NgModule({
+  declarations: [DashboardPageComponent],
+  imports: [
+    RouterModule.forChild(routes),
+    CommonModule,
+    FormsModule,
+    ChartModule,
+    MenuModule,
+    TableModule,
+    StyleClassModule,
+    PanelMenuModule,
+    ButtonModule,
+  ],
+  exports: [DashboardPageComponent],
+})
+export class DashboardModule {}
