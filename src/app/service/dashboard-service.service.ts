bb59d26665ca95303aea092f788968d7be30e40e
Abdelrahman Elemary <46689496+BoodyElemary@users.noreply.github.com>
2023-07-04 00:07:16 +0300

    finsihing admin chart

--- src/app/service/dashboard-service.service.ts
+++ src/app/service/dashboard-service.service.ts
@@ -1,11 +1,22 @@
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { LoginService } from './login.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class DashboardServiceService {
-  apiUrl = `${environment.apiUrl}/customers`;
-  constructor() {}
+  apiUrl = `${environment.apiUrl}/orders`;
+  constructor(
+    private httpServ: HttpClient,
+    private loginService: LoginService
+  ) {}
+  getSingleStoreInsights() {
+    const token = this.loginService.getToken();
+    const headers = new HttpHeaders().set('Authorization', `${token}`);
+    return this.httpServ.get(`${this.apiUrl}/store/insights`, {
+      headers,
+    });
+  }
 }
